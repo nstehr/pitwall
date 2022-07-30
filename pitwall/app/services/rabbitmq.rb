266636c78a00b328ad4c55cf6c71b996956aa0fa
Nathan Stehr <nstehr@gmail.com>
2022-07-29 23:34:21 -0400

    remove unused class method

--- pitwall/app/services/rabbitmq.rb
+++ pitwall/app/services/rabbitmq.rb
@@ -3,10 +3,6 @@ require 'bunny'
 class Rabbitmq
     @@exchange = "pitwall.orchestration"
 
-    def self.exchange
-        @@exchange
-    end
-
     def send(routing_key, message)
         conn = Bunny.new.tap(&:start)
         ch = conn.create_channel
