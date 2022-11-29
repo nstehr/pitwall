20bc374aafa02a54969b2bde8e548b5760c86719
Nathan Stehr <nstehr@gmail.com>
2022-11-28 21:19:30 -0500

    adding bootstrap and pagy for paging

--- pitwall/config/initializers/pagy.rb
+++ pitwall/config/initializers/pagy.rb
@@ -1,3 +1,5 @@
+require 'pagy/extras/bootstrap'
+
 Pagy::DEFAULT[:page] = 1 # default page to start with
 Pagy::DEFAULT[:items] = 25 # items per page
 Pagy::DEFAULT[:cycle] = false # when on last page, click "Next" to go to first page
