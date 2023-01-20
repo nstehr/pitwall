221444ec14bef11e7a026cd8370c57022aac5d0c
Nathan Stehr <nstehr@gmail.com>
2023-01-19 22:40:18 -0500

    erb formatting plus adding some basic bootstrap styling to vm and orchestrators pages

--- pitwall/app/helpers/orchestrators_helper.rb
+++ pitwall/app/helpers/orchestrators_helper.rb
@@ -1,2 +1,12 @@
 module OrchestratorsHelper
+        def get_orch_status_color(status)
+           case status
+           when "UP"
+            "#65c3ba"
+           when "DOWN"
+            "#4a4e4d"
+           else
+            "#e6e6ea"
+        end
+    end
 end
--- pitwall/app/helpers/virtual_machines_helper.rb
+++ pitwall/app/helpers/virtual_machines_helper.rb
@@ -1,2 +1,22 @@
 module VirtualMachinesHelper
+    def get_status_color(status)
+       case status
+       when "RUNNING"
+        "#65c3ba"
+       when "INIT"
+        "#e6e6ea"
+       when "STOPPED"
+        "#4a4e4d"
+       when "STOPPING"
+        "#4a4e4d"
+       when "ERROR"
+        "#fe4a49"
+       when "BOOTING"
+        "#8874a3"
+       when "BUILDING_FILESYSTEM"
+        "#8874a3"
+       else
+        "#e6e6ea"
+    end
+end
 end
