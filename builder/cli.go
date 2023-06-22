9e54ab582d7c3a79680b3b81a545c036d1ce49a3
Nathan Stehr <nstehr@gmail.com>
2023-06-21 22:25:02 -0400

    build a few binaries for the cli

--- builder/cli.go
+++ builder/cli.go
@@ -2,22 +2,34 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"dagger.io/dagger"
 )
 
 func buildCli(ctx context.Context, client *dagger.Client, project *dagger.Directory, platform dagger.Platform) error {
 
-	builder := client.Container(dagger.ContainerOpts{Platform: platform}).From("golang:latest")
-	builder = builder.WithMountedDirectory("/src", project).WithWorkdir("/src")
+	oses := []string{"linux", "darwin"}
+	arches := []string{"amd64", "arm64"}
+	b := client.Container(dagger.ContainerOpts{Platform: platform}).From("golang:latest")
+	b = b.WithMountedDirectory("/src", project).WithWorkdir("/src")
 	path := "dist"
-	builder = builder.WithExec([]string{"go", "build", "-o", "dist/pwctl"})
 
-	output := builder.Directory(path)
+	for _, goos := range oses {
+		for _, goarch := range arches {
+			builder := b.WithEnvVariable("GOOS", goos)
+			builder = builder.WithEnvVariable("GOARCH", goarch)
 
-	_, err := output.Export(ctx, path)
-	if err != nil {
-		return err
+			builder = builder.WithExec([]string{"go", "build", "-o", fmt.Sprintf("dist/%s/%s/pwctl", goos, goarch)})
+
+			output := builder.Directory(path)
+
+			_, err := output.Export(ctx, path)
+			if err != nil {
+				return err
+			}
+		}
 	}
+
 	return nil
 }
